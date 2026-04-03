90a18a5a8f11a3e9cd19c41ce18471eb5bf426cf
TungSkipli <timle.engineer@gmail.com>
2026-04-03 20:31:54 +0700

    update

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { createServer } from "http";
 import app from "./src/server/index.js";
-import { initSocketIO } from "./src/lib/socketHandler.js";
 
 const PORT = process.env.PORT || 8080;
 
-// Create HTTP server (required for Socket.io to share the same port)
-const httpServer = createServer(app);
-
-// Initialize Socket.io on the HTTP server
-initSocketIO(httpServer);
-
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
